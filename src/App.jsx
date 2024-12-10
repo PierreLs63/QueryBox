bd8f7b39f8bd7a98f8e5992c7d374b917c3eefea
CCtuhulu <wajdimhamdi999@gmail.com>
2024-12-10 09:32:00 +0100

    Enviornment setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'antd';
+
+const App = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Welcome to Ant Design with Vite!</h1>
+      <Button type="primary">Click Me</Button>
+    </div>
+  );
+};
+
+export default App;
