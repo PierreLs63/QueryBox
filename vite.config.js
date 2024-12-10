994a97cf34e6ad2140317befc441b68255677cbf
Xuan <zxuuuan0321@gmail.com>
2024-12-10 09:58:10 +0100

    ajouter multipages config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        inscription: './src/pages/inscription/index.html',
+        connexion: './src/pages/connexion/index.html',
+        reinitialiser: './src/pages/reinitialiser/index.html',
+        mailenvoye: './src/pages/mailenvoye/index.html',
+        mailverifie: './src/pages/mailverifie/index.htnl'
+      },
+    },
+  },
 })
