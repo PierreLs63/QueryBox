0c04c2b117b290ed31778d17e8ad5f775fc7d1b7
Xuan <zxuuuan0321@gmail.com>
2024-12-11 08:27:12 +0100

    compelet config

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,13 @@ export default defineConfig({
         connexion: './src/pages/connexion/index.html',
         reinitialiser: './src/pages/reinitialiser/index.html',
         mailenvoye: './src/pages/mailenvoye/index.html',
-        mailverifie: './src/pages/mailverifie/index.htnl'
+        mailverifie: './src/pages/mailverifie/index.html',
+        debutant: './src/pages/debutant/index.html',
+        tuto1: './src/pages/tuto1/index.html',
+        tuto2: './src/pages/tuto2/index.html',
+        tuto3: './src/pages/tuto3/index.html',
+        tuto4: './src/pages/tuto4/index.html',
+        acceuil: './src/pages/acceuil/index.html'
       },
     },
   },
