78bde4911d2bca22c9f8f525618ce53c95de9be5
PierreLs63 <pierrelouis.vidal@hotmail.fr>
2024-12-17 09:11:50 +0100

    hook to create a workspace

---
+++ src/hooks/workspace/useCreate.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import toast from 'react-hot-toast';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const useCreate = () => {
+    const [workspace, setWorkspace] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(null);
+    const api = `http://localhost:5001/api/${process.env.VERSION || "v1"}/workspace/create`;
+    const createWorkspace = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch(api, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const data = await response.json();
+            if (data.error) {
+                throw new Error(data.error);
+            }
+            setWorkspace(data);
+            setSuccess(data.message);
+        }
+        catch (error) {
+            setError(error.message);
+            toast.error(error.message);
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+    return { workspace, loading, error, success, createWorkspace }
+}
+
+export default useCreate
