3581260f7d59d8e227d1a3dbf5242f94f1ed7f4f
PierreLs63 <pierrelouis.vidal@hotmail.fr>
2024-12-18 09:45:55 +0100

    response hook

---
+++ src/hooks/response/useDelete.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import toast from 'react-hot-toast';
+import baseURL from '../../utils/variables';
+
+const useDelete = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(null);
+    const [responseId, setResponseId] = useState(null);
+
+    const deleteResponse = async (responseId) => {
+        setResponseId(responseId);
+        setLoading(true);
+        setError(null);
+        setSuccess(null);
+        const api = `${baseURL}/response/${responseId}`;
+        try {
+            const response = await fetch(api, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const data = await response.json();
+            if (data.error) {
+                throw new Error(data.error);
+            }
+            setSuccess(data.message);
+        }
+        catch (error) {
+            setError(error.message);
+            toast.error(error.message);
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+    return { loading, error, success, deleteResponse, responseId }
+}
+
+export default useDelete
