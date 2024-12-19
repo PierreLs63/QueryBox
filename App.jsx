8a719eb5c8db191ae4933c4293f1bd4a0b4d4bc7
Xuan <zxuuuan0321@gmail.com>
2024-12-19 09:00:31 +0100

    add navigation of connexion inscription and reinitialiser

--- App.jsx
+++ App.jsx
@@ -6,31 +6,33 @@ import Mailverifie from './src/pages/mailverifie/Mailverifie.jsx'
 import Accueil from './src/pages/accueil/Accueil.jsx'
 import Debutant from './src/pages/debutant/Debutant.jsx'
 import Tuto from './src/pages/tuto/Tuto.jsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
       <Toaster toastOptions={{
-      // Define default options
-      className: 'font-bold',
-      duration: 5000}} />
+        // Define default options
+        className: 'font-bold',
+        duration: 5000,
+      }} />
       <Router>
-          <Routes>
-              <Route path="/connexion" element={<Connexion />} />
-              <Route path="/inscription" element={<Inscription />} />
-              <Route path="/reinitialiser" element={<Reinitialiser />} />
-              <Route path="/mailenvoye" element={<Mailenvoye />} />
-              <Route path="/mailverifie" element={<Mailverifie />} />
-              <Route path="/accueil" element={<Accueil />} />
-              <Route path="/debutant" element={<Debutant />} />
-              <Route path="/tuto" element={<Tuto />} />
-          </Routes>
+        <Routes>
+          {/* default path navigate to /connexion */}
+          <Route path="/" element={<Navigate to="/connexion" replace />} />
+          <Route path="/connexion" element={<Connexion />} />
+          <Route path="/inscription" element={<Inscription />} />
+          <Route path="/reinitialiser" element={<Reinitialiser />} />
+          <Route path="/mailenvoye" element={<Mailenvoye />} />
+          <Route path="/mailverifie" element={<Mailverifie />} />
+          <Route path="/accueil" element={<Accueil />} />
+          <Route path="/debutant" element={<Debutant />} />
+          <Route path="/tuto" element={<Tuto />} />
+        </Routes>
       </Router>
     </>
-  )
-  
+  );
 }
 
-export default App
+export default App;
