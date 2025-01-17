e5abdb6ad21fc06d07c28aeb5f6126f2647a7d0f
Younes Ahrioui <younes.ahrioui@insa-lyon.fr>
2025-01-17 08:20:15 +0100

    paramRequest and response key value not required

--- models/Response.js
+++ models/Response.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 // Key value structure to be implemented in header with disabled id
 const KeyValueSchema = new mongoose.Schema({
-    key: { type: String, required: true },
-    value: { type: String, required: true }
+    key: { type: String },
+    value: { type: String }
   }, { _id: false });
 
 
