f631d27404ade04e1fdc47036628fbb80a463424
Xuan <zxuuuan0321@gmail.com>
2025-01-17 08:46:57 +0100

    change mailverifie style

--- src/pages/mailverifie/Mailverifie.jsx
+++ src/pages/mailverifie/Mailverifie.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect }  from 'react';
+import React from 'react';
 import { Button } from 'antd';
 import { useNavigate } from 'react-router-dom';
+import './Mailverifie.css'
 
 const Mailverifie = () => {
   // Navigate to debutant
@@ -8,72 +9,19 @@ const Mailverifie = () => {
   const handleMailVerifie = () => {
     navigate('/debutant');
   };
-
-  //Change default param from index.css
-  useEffect(() => {
-    document.body.style.margin = '0';
-    document.body.style.padding = '0';
-    document.body.style.height = '100vh';
-    document.body.style.backgroundColor = '#e6f7ff';
-    document.body.style.display = 'flex';
-    document.body.style.justifyContent = 'center';
-    document.body.style.alignItems = 'center';
-  }, []);
-
-  const styles = {
-    global: {
-      margin: '0',
-      padding: '0',
-      boxSizing: 'border-box',
-      height: '100vh',
-      backgroundColor: '#e6f7ff',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      flexDirection: 'column',
-    },
-    logo: {
-      fontFamily: 'Monofett',
-      fontSize: '70px',
-      fontWeight: 'bold',
-      color: '#1890FF',
-      marginBottom: '30px',
-    },
-    message: {
-      fontSize: '20px',
-      color: '#060606',
-      marginBottom: '30px',
-      textAlign: 'center',
-    },
-    button: {
-      borderRadius: '24px',
-      backgroundColor: '#1890FF',
-      color: '#fff',
-      border: 'none',
-      padding: '10px 20px',
-      fontSize: '16px',
-      cursor: 'pointer',
-      transition: 'background-color 0.3s',
-    },
-    buttonHover: {
-      backgroundColor: '#106cc8',
-    },
-  };
-
+  
   return (
-    <div style={styles.global}>
-      <div style={styles.logo}>
+    <div className='global'>
+      <div className='logo'>
         QueryBox
       </div>
-      <div style={styles.message}>
+      <div className='message'>
         Votre mail a bien été validé.
       </div>
       <Button
         type="primary"
-        style={styles.button}
+        className='button'
         onClick={handleMailVerifie}
-        onMouseOver={(e) => (e.target.style.backgroundColor = styles.buttonHover.backgroundColor)}
-        onMouseOut={(e) => (e.target.style.backgroundColor = styles.button.backgroundColor)}
       >
         Aller à la page d'accueil
       </Button>
