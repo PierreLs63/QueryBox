ff3e9af33136b6fe1d827459d9c2fdf593ad5da3
PierreLs63 <pierrelouis.vidal@hotmail.fr>
2025-01-22 10:15:29 +0100

    bug fix

--- src/hooks/requests/useRequestNameById.jsx
+++ src/hooks/requests/useRequestNameById.jsx
@@ -24,8 +24,8 @@ const useRequestNameById = () => {
                 toast.error(data.message);
                 setErroeRequestNameById(data.message);
             }
-            CurrentState.setCollectionName(data.name)
-            setSucceseRequestNameById(data.name);
+            CurrentState.setRequestName(data.request.name)
+            setSucceseRequestNameById(data.request.name);
         }
         catch (error) {
             setErroeRequestNameById(error.message);
