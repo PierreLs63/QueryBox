2ad3805e82c4dfcc492e001975abc826309722bc
PierreLs63 <pierrelouis.vidal@hotmail.fr>
2025-01-22 09:31:25 +0100

    creation breadCrumb

---
+++ src/components/breadCrumb.jsx
@@ -0,0 +1,41 @@
+import { Breadcrumb } from 'antd';
+import { HomeOutlined, AppstoreOutlined, TeamOutlined, FileTextOutlined } from '@ant-design/icons';
+import useCurrentState from '../zustand/CurrentState';
+
+const BreadCrumb = () => {
+    const { workspaceId, collectionName, requestName, workspaceName } = useCurrentState();
+    const breadcrumbItems = [
+        {
+            icon: <HomeOutlined />,
+            text: 'Home'
+            },
+        {
+            icon: <AppstoreOutlined />,
+            text: workspaceName
+            },
+        {
+            icon: <TeamOutlined />,
+            text: collectionName
+            },
+        {
+            icon: <FileTextOutlined />,
+            text: requestName
+            }
+    ]
+    return (
+        <div>
+            {workspaceId !== null && (
+                <Breadcrumb>
+                    {breadcrumbItems.map((item, index) => (
+                        <Breadcrumb.Item key={index} href="#">
+                            {item.icon}
+                            <span>{item.text}</span>
+                        </Breadcrumb.Item>
+                    ))}
+                </Breadcrumb>
+            )}
+        </div>
+    )
+}
+
+export default BreadCrumb;
